a1645b9e7fa1896e6220a633b925f0df87a7a9c8
themaryanjuguna <themaryanjugunaagmail.com>
2022-06-07 14:49:38 +0300

    API key and API base URL Connected

---
+++ app/src/main/java/com/maryannenjuguna/thecatapi/Constants.java
@@ -0,0 +1,10 @@
+package com.maryannenjuguna.thecatapi;
+
+public class Constants {
+
+    public static final String THECATAPI_BASE_URL = "https://api.thecatapi.com/v1/";
+    //public static final String THECATAPI_KEY = BuildConfig.THECATAPI_KEY;
+    public static final String THECATAPI_KEY = "624a113a-c995-4b7e-bd5e-d2d6d200e37f";
+
+
+}
