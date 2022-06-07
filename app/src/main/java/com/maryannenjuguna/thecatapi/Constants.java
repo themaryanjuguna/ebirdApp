2e3aa7b6f0858d7cde5c6873bba65ba4130189c5
themaryanjuguna <themaryanjugunaagmail.com>
2022-06-07 14:15:12 +0300

    custom folders and files created

---
+++ app/src/main/java/com/maryannenjuguna/thecatapi/Constants.java
@@ -0,0 +1,4 @@
+package com.maryannenjuguna.thecatapi;
+
+public class Constants {
+}
--- app/src/main/java/com/maryannenjuguna/thecatapi/ui/MainActivity.java
+++ app/src/main/java/com/maryannenjuguna/thecatapi/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.maryannenjuguna.thecatapi;
+package com.maryannenjuguna.thecatapi.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.maryannenjuguna.thecatapi.R;
 
 public class MainActivity extends AppCompatActivity {
 
