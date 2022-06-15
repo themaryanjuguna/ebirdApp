a502e73ddc4e2da21faac37915086e5d967f7c5e
themaryanjuguna <themaryanjugunaagmail.com>
2022-06-15 19:51:46 +0300

    UI Style Updated

--- app/src/main/java/com/maryannenjuguna/thecatapi/ui/MainActivity.java
+++ app/src/main/java/com/maryannenjuguna/thecatapi/ui/MainActivity.java
@@ -30,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         recyclerView = findViewById(R.id.viewRecycler);
         theCatApi feral = theCatApiClient.getClient();
         Call<List<TheCatBreedSearchResponse>> catFamily = feral.getBreeds();
