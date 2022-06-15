5f3f9c9e17436e082111cd624fdb03b24e51e399
themaryanjuguna <themaryanjugunaagmail.com>
2022-06-16 00:43:50 +0300

    Unused adapter file & unncessary code removed.

--- app/src/main/java/com/maryannenjuguna/thecatapi/ui/BreedsActivity.java
+++ app/src/main/java/com/maryannenjuguna/thecatapi/ui/BreedsActivity.java
@@ -2,29 +2,6 @@ package com.maryannenjuguna.thecatapi.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import com.maryannenjuguna.thecatapi.R;
-import com.maryannenjuguna.thecatapi.TheCatApiBreedsArrayAdapter;
-import com.maryannenjuguna.thecatapi.models.Image;
-import com.maryannenjuguna.thecatapi.models.TheCatBreedSearchResponse;
-import com.maryannenjuguna.thecatapi.network.theCatApi;
-import com.maryannenjuguna.thecatapi.network.theCatApiClient;
-
-import java.util.List;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 public class BreedsActivity extends AppCompatActivity {
 
 
