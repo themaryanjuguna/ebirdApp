2aad106c8744b5e2198f95f2d742b7798713a14a
themaryanjuguna <themaryanjugunaagmail.com>
2022-06-21 02:12:03 +0300

    Lottie animated splash screen

--- app/src/main/java/com/maryannenjuguna/thecatapi/ui/SplashActivity.java
+++ app/src/main/java/com/maryannenjuguna/thecatapi/ui/SplashActivity.java
@@ -5,17 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.maryannenjuguna.thecatapi.R;
 import com.maryannenjuguna.thecatapi.ui.loginsignup.LoginActivity;
 import com.maryannenjuguna.thecatapi.ui.loginsignup.RegisterActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    boolean islottie = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        LottieAnimationView lottie = findViewById(R.id.lottie);
+        lottie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(islottie){
+                    lottie.setSpeed(-1);
+                    lottie.playAnimation();
+                    islottie = false;
+                } else{
+
+                    lottie.setSpeed(2);
+                    lottie.playAnimation();
+                    islottie = true;
+                }
+            }
+        });
 
 
         Handler handler = new Handler();
@@ -23,11 +45,11 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 //calling main activity to load after loading screen
-                startActivity(new Intent(SplashActivity.this, SetupActivity.class));
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
                 finish(); //finish loading screen activity
             }
             //call mainActivity after 1 seconds
-        }, 2500);
+        }, 5000);
 
     }
     }
